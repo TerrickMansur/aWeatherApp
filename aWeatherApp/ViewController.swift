097ca0e8bb581d8eb406ce687f65eb26ab0a70bd
Terrick Mansur <terrickmansur@gmail.com>
2019-06-08 15:50:17 +0200

    Creates project

---
+++ aWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aWeatherApp
+//
+//  Created by Terrick Mansur on 6/8/19.
+//  Copyright © 2019 Terrick Mansur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
